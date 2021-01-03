ddf8d6b9c6a092ea5d15dad270b6babc704d2d11
nwemar <capital.nwemarwin@gmail.com>
2021-01-03 13:17:19 +0630

    first commit

---
+++ CoreApi/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreApi
+{
+    public class Book
+    {
+        public int id { get; set; }
+        public string Title { get; set; }
+        public Nullable<int> Price { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ CoreApi/Controllers/BookController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreApi.Controllers
+{
+    [Route("api/[controller]/[Action]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        [HttpGet]
+        [ActionName("GetBookList")]
+        public IEnumerable<Book> GetBookList()
+        {
+            List<Book> book_Lst = new List<Book>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Book obj = new Book();
+                obj.id = i + 1;
+                if (i == 0)
+                    obj.Title = "C#";
+                if (i == 1)
+                    obj.Title = "PHP";
+                if (i == 2)
+                    obj.Title = "Ruby";
+                obj.Price = 500;
+                obj.Description = obj.Title + " BOOK";
+                book_Lst.Add(obj);
+            }
+            return book_Lst.ToList();
+        }
+    }
+}
